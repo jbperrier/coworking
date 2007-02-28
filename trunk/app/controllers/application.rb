7ca8a65932d84989ae85901ba0271683e0cb71f4
phil.leif <phil.leif@2c86eca3-4f2a-0410-97f1-37b534e648cb>
2007-02-28 04:06:56 +0000

    restyled

--- trunk/app/controllers/application.rb
+++ trunk/app/controllers/application.rb
@@ -4,7 +4,7 @@
 class ApplicationController < ActionController::Base
   # Pick a unique cookie name to distinguish our session data from others'
   session :session_key => '_Coworker_session_id'
-  layout 'default'
+  layout 'sydney'
   require 'twitter'
   include AuthenticatedSystem
 end
--- trunk/app/controllers/users_controller.rb
+++ trunk/app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+  def icon
+    begin
+      statuses = Twitter::Base.new('phil@coutorture.com', 'coworking').followers
+      for status in statuses
+        if status.user.screen_name == params[:id]
+          render :layout => false, :inline => '<img src="' + status.user.profile_image_url + '" />'
+        end
+      end
+    rescue
+    end
+    render :layout => false, :inline => '<img src="http://assets1.twitter.com/system/user/profile_image/795695/normal/Ritual_Working_128.png" />'
+  end
 end
