a0b3f9cf71e1b5664b86e781cc904e7a46de21aa
phil.leif <phil.leif@2c86eca3-4f2a-0410-97f1-37b534e648cb>
2007-03-02 04:04:44 +0000

    changed account redirects

--- trunk/app/controllers/account_controller.rb
+++ trunk/app/controllers/account_controller.rb
@@ -17,7 +17,7 @@ class AccountController < ApplicationController
         self.current_user.remember_me
         cookies[:auth_token] = { :value => self.current_user.remember_token , :expires => self.current_user.remember_token_expires_at }
       end
-      redirect_back_or_default(:controller => '/spaces', :action => 'index')
+      redirect_back_or_default(:controller => '/home', :action => 'index')
       flash[:notice] = "Logged in successfully"
     end
   end
@@ -27,7 +27,7 @@ class AccountController < ApplicationController
     return unless request.post?
     @user.save!
     self.current_user = @user
-    redirect_back_or_default(:controller => '/spaces', :action => 'index')
+    redirect_back_or_default(:controller => '/home', :action => 'index')
     flash[:notice] = "Thanks for signing up!"
   rescue ActiveRecord::RecordInvalid
     render :action => 'signup'
@@ -38,6 +38,6 @@ class AccountController < ApplicationController
     cookies.delete :auth_token
     reset_session
     flash[:notice] = "You have been logged out."
-    redirect_back_or_default(:controller => '/spaces', :action => 'index')
+    redirect_back_or_default(:controller => '/home', :action => 'index')
   end
 end
