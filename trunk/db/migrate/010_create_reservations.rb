972361fe6c1dc1e70368944e6853919dd88ccfb5
phil.leif <phil.leif@2c86eca3-4f2a-0410-97f1-37b534e648cb>
2007-03-02 03:03:17 +0000

    fixed typo in reservation migration

--- trunk/db/migrate/010_create_reservations.rb
+++ trunk/db/migrate/010_create_reservations.rb
@@ -1,7 +1,7 @@
 class CreateReservations < ActiveRecord::Migration
   def self.up
     create_table :reservations do |t|
-      t.column :date, :datetime
+      t.column :date, :date
       t.column :user_id, :integer
       t.column :space_id, :integer
       t.column :note, :text
